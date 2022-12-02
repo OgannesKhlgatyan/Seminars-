8afcce29ae0ab8b020a98a9269ea1e569f1febf5
Ogannes <Vovan91146@mail.ru>
2022-12-02 21:58:57 +0300

    Исправили задачу 2.3

--- Lesson#2.3/Program.cs
+++ Lesson#2.3/Program.cs
@@ -2,11 +2,20 @@
 Console.WriteLine("Введите число, а я проверю кратно ли оно 7 и 23 одновременно");
 int number = int.Parse(Console.ReadLine());
 
-double num = (double)number;
-
-if (num % 7 == 0 && num % 23 == 0)
+if (number % 7 == 0 && number % 23 == 0)
 {
     Console.Write("Число одновременно кратно 7 и 23");
 }
 else 
 Console.Write("Число " + number + " НЕ кратно 7 и 23 одновременно");
+
+// bool isParsed = false;
+// int number = 0;
+// while (isParsed == false)
+// {
+//     isParsed = int.TryParse(Console.ReadLine(), out number);
+//     if(isParsed == false)
+//     {
+//         Console.WriteLine("Пиши числа!");
+//     }
+// }
