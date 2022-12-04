8bc5850e66fac29f6cf09cde61c3658bef1b1f19
Ogannes <Vovan91146@mail.ru>
2022-12-04 16:13:55 +0300

    Находим вторую цифру трехзначного числа

---
+++ Homework#S2/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число: ");
+int number = 0;
+bool isParsed = false;
+while (isParsed == false)
+{
+    isParsed = int.TryParse(Console.ReadLine(), out number);
+    if (isParsed == false)
+    {
+        Console.WriteLine("Пожалуйста, введите трехзначное число!");
+    }
+}
+if (number/100 < 10 && number/100 >= 1 || number/100 > -10 && number/100 <= -1)
+{
+    int result = (number/10) % 10;
+    Console.WriteLine("Ваше число: " + number);
+    Console.WriteLine("Вторая цифра : " + Math.Abs(result));
+}
+else 
+{
+    Console.WriteLine("Число не трехзначное!!!");
+}
