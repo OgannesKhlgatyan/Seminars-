dd1e0f93eb14e9be3d592964e9b011a9b9f72d77
Ogannes <Vovan91146@mail.ru>
2022-12-06 21:54:56 +0300

    Узнаем в какой части находится точка

---
+++ Lesson#3.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача No18. Общее обсуждение
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int quarter = int.Parse(Console.ReadLine());
+string result = GetRangeByNumberOfQuarter(quarter);
+Console.WriteLine(result);
+
+string GetRangeByNumberOfQuarter(int numberOfQuarter)
+{
+    if (numberOfQuarter == 1)
+    {
+        return "(x > 0 && y > 0)";
+    }
+
+    else if (numberOfQuarter == 2)
+    {
+        return "(x < 0 && y > 0)";
+    }
+
+    else if (numberOfQuarter == 3)
+    {
+        return "(x < 0 && y < 0)";
+    }
+
+    else if (numberOfQuarter == 4)
+    {
+        return "(x > 0 && y < 0)";
+    }
+
+    return "-1";
+}
