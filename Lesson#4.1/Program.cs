e77b7da13bea8731a83c079cbb986a52a78def38
Ogannes <Vovan91146@mail.ru>
2022-12-09 20:43:32 +0300

    Считаем колличество цифр в числе

---
+++ Lesson#4.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3 78 -> 2 89126 -> 5
+
+int num = int.Parse(Console.ReadLine());
+int result = CheckCountNumber(num);
+Console.WriteLine(result);
+
+
+int CheckCountNumber(int number)
+{
+    int count = 0;
+    for (int i = 1; number > 0; i++)
+    {
+        number = number / 10;
+        count = count + 1;
+    }
+    return count;
+}
