4bba9059279fc4cd95889bd7c5594076950d9365
Ogannes <Vovan91146@mail.ru>
2022-12-09 20:55:14 +0300

    Произведение от 1 до А

---
+++ Lesson#4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 24: Напишите программу, которая принимает на вход число (А) 
+//и выдаёт произведение чисел от 1 до А.
+
+int a = int.Parse(Console.ReadLine());
+double comp = GetСompositionNumbersBetweenOneToNumber(a);
+Console.WriteLine(comp);
+
+double GetСompositionNumbersBetweenOneToNumber(int a)
+{
+    int comp = 1;
+    for (var i = 1; i <= a; i++)
+    {
+        comp = comp * i;
+    }
+
+    return comp;
+}
