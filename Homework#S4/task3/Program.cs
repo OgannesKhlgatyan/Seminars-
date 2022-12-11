f8cfee13c90e1953244b4d745467a6140165d846
Ogannes <Vovan91146@mail.ru>
2022-12-11 16:59:57 +0300

    задаёт массив из N элементов и выводит их на экран

---
+++ Homework#S4/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Укажите размер массива:");
+int[] n = new int[int.Parse(Console.ReadLine())]; // int.Parse(Console.ReadLine()) предлагаю ввести размер массива с консоли
+Console.WriteLine("Заполите массив целыми числами:");
+GetIncomeNumbers(n);  // Использую метод для заполнения массива
+PrintArray(n); // Использую метод для вывода массива
+
+void GetIncomeNumbers(int[] n)
+{
+    for (int i = 0; i < n.Length; i++)
+    {
+        n[i] = int.Parse(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
