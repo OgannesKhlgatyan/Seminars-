88e3c39e932e6b0d058796a4433f37604e961e10
Ogannes <Vovan91146@mail.ru>
2022-12-11 15:21:35 +0300

    Цикл возведения А в степень B

---
+++ Homework#S4/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать метод Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.WriteLine("Пожалуйста, введите 1-е число:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите 2-е число:");
+int B = int.Parse(Console.ReadLine());
+double result = RaiseToADegree(A, B);
+Console.WriteLine(result + " является результатом возведения числа A: " + A + ", в степень B: " + B);
+
+
+double RaiseToADegree(int A, int B)
+{
+    int i = 0;
+    double mnozh = 1;
+
+    if (B < 0)
+    {
+        while (i > B)
+        {
+            mnozh = mnozh * A;
+            i--;
+        }
+        mnozh = 1/mnozh;
+        return mnozh;
+    }   
+    else
+    {
+        while (i < B)
+        {
+            mnozh = mnozh * A;
+            i++;
+        }
+    }
+    return mnozh;
+}
