1f8337994ee43f2450e76c74f109635f2e6735b2
Ogannes <Vovan91146@mail.ru>
2022-12-11 16:22:59 +0300

    Принимает число и выдаёт сумму цифр в числе

---
+++ Homework#S4/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int N = int.Parse(Console.ReadLine());
+int result = GetSummChars(N);
+Console.WriteLine(result);
+
+int GetSummChars(int N)
+{
+    int i;
+    int summ = 0;
+    for (i = 0; N != 0; i++)
+    {
+        summ = N % 10 + summ;
+        N = N / 10;
+    }
+    return Math.Abs(summ);
+}
