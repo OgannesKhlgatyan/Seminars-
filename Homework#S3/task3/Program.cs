49104f2e3f232da988c26ffdfe556436bdddc072
Ogannes <Vovan91146@mail.ru>
2022-12-11 14:34:18 +0300

    Вывод кубов чисел от 1 до А

---
+++ Homework#S3/task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int num = int.Parse(Console.ReadLine());
+int[] arr = GetCalculateTheСubesOfnumbers(num);
+
+PrintArray(arr);
+
+int[] GetCalculateTheСubesOfnumbers(int N)
+{
+    int[] array = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
