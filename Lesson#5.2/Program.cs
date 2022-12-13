c4a6493d2f58a87f03b4a2626f62297d9d630dee
Ogannes <Vovan91146@mail.ru>
2022-12-13 22:37:24 +0300

    Поиск числа в массиве

---
+++ Lesson#5.2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+int[] array = CreateRandomArray(4, -9, 9);
+PrintArray(array, "Random Array");
+Console.WriteLine("Enter a number:");
+bool find = FindItem(array, int.Parse(Console.ReadLine()));
+Console.WriteLine(find);
+
+
+bool FindItem(int[] array, int number)
+{
+    for (var i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[] array = new int[size];
+
+    for (var i = 0; i < size; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array, string message)
+{
+    string str = string.Join(", ", array);
+    Console.WriteLine($"{message} - [ {str} ]");
+}
